8a712fca3864adad12caa01a1c15d15e2e47e515
dlaekduddlaekdud <ekdud0112@hufs.ac.kr>
2025-07-04 19:38:47 +0900

    Title: 최소공배수 구하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250704/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+
+gcd = 1
+for i in range(1, min(n, m) + 1):
+    if n % i == 0 and m % i == 0:
+        gcd = i
+
+# 최소공배수 구하기
+lcm = (n * m) // gcd
+
+print(lcm)

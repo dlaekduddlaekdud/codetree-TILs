8c52760cc739b3a1e611bb928a79b6554b858b6d
dlaekduddlaekdud <ekdud0112@hufs.ac.kr>
2025-07-16 19:51:37 +0900

    Title: 함수를 이용한 윤년 판별, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250716/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 250716/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -6,7 +6,7 @@ def f(y):
         if y % 100 == 0 and y % 400 != 0:
             return 'false'
         else:
-            reutrn 'true'
+            return 'true'
     else:
         return 'false'
         
